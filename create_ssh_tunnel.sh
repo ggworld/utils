c3439ecb9cee903109cda190274e55b5dc98b77e
Menachem Geva <me.geva@gmail.com>
2020-09-16 14:53:19 +0300

    add g

--- create_ssh_tunnel.sh
+++ create_ssh_tunnel.sh
@@ -1,6 +1,6 @@
 #in the background 
-#               database url                 tunnel ip        public key 
-ssh -N -L 3306:10.10.10.99:3306  menachem@x.x.x.x -i k_g_1f1 -f
+#               database url                 tunnel ip  public key -g yes yo except connections on all Ips  
+ssh -N -L 3306:10.10.10.99:3306  menachem@x.x.x.x -i k_g_1f1 -f -g yes
 
 
 #open prompt:
