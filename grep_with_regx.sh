0c556d92cc0a6ffbe7e7f2d39f97b3450d62cf3e
Menachem Geva <me.geva@gmail.com>
2021-07-14 01:13:16 +0300

    Update grep_with_regx.sh

--- grep_with_regx.sh
+++ grep_with_regx.sh
@@ -1,1 +1,5 @@
- grep -io --color '[A-Z]*_tape' *.json | sort -u
+# find all with file name and display mach once per file
+grep -io --color '[A-Z]*_tape' *.json | sort -u
+
+#find only the combinations 
+grep -io --color '[A-Z]*_tape' *.json | awk -F':' '{print $2}'  | sort -u
