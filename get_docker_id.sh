5cea4e654d8b0cfe5184128fc25de5c12b668e14
Menachem Geva <me.geva@gmail.com>
2022-03-22 16:46:35 +0200

    Update get_docker_id.sh

--- get_docker_id.sh
+++ get_docker_id.sh
@@ -2,3 +2,6 @@ docker ps | grep "worker" | awk '{print $1}'
 
 # and with Sasha insight:
 docker exec -it $(docker ps | grep "worker" | awk '{print $1}' | xargs -I% echo "%") bash
+
+# and with user airflow
+docker exec -it -u airflow $(docker ps | grep "worker" | awk '{print $1}' | xargs -I% echo "%") bash
