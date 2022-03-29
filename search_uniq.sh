de4362984aad08fe10a99059825822416738da30
Menachem Geva <me.geva@gmail.com>
2022-03-29 15:08:09 +0300

    Update search_uniq.sh

--- search_uniq.sh
+++ search_uniq.sh
@@ -1,4 +1,4 @@
 grep -i -o -m 1 -P '.{0,10}material_type' *.json | awk -F '::' '{print $2}' | sort | uniq
 
 # find files that containes value
-grep -c dbt *.py |  awk -F: '$NF+0 > 1'
+grep -c dbt *.py |  awk -F: '$NF+0 > 0'
