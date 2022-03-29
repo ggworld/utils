13931556310a9d0e846fd598df1d6d597fe48787
Menachem Geva <me.geva@gmail.com>
2022-03-29 15:11:23 +0300

    Update search_uniq.sh

--- search_uniq.sh
+++ search_uniq.sh
@@ -2,3 +2,7 @@ grep -i -o -m 1 -P '.{0,10}material_type' *.json | awk -F '::' '{print $2}' | so
 
 # find files that containes value
 grep -c dbt *.py |  awk -F: '$NF+0 > 0'
+
+# and get the files date:
+ls -ltr $(grep -c dbt *.py |  awk -F: '$NF+0 > 0' | awk 'BEGIN { FS = ":" } ; { print $1 }' )
+
