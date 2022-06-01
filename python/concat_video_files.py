9fbad2a279f7b3f0024a3c8f63995bbac5fca816
Hi-Data <106279640+Hi-Data@users.noreply.github.com>
2022-06-01 15:56:51 +0300

    Create concat_video_files.py

---
+++ python/concat_video_files.py
@@ -0,0 +1,14 @@
+#good source and example for this package : https://www.programcreek.com/python/example/105718/moviepy.editor.VideoFileClip
+
+from moviepy.editor import *
+
+
+#looping on all video files named 1-7 
+for i in range(1,8):
+    my_list[i]=VideoFileClip(f"/Users/geva/Documents/private/tmp/WhatsApp{i}.mp4")
+
+
+new_clip = concatenate_videoclips(list(my_list.values()))
+
+#Saving the new combine file 
+new_clip.write_videofile("/Users/geva/Downloads/test3.mp4", temp_audiofile="temp-audio.m4a", remove_temp=True, codec="libx264", audio_codec="aac")
