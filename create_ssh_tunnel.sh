a6384aec749e6e5e8ac92824046bf09a0faa0645
Menachem Geva <me.geva@gmail.com>
2022-08-29 14:37:47 +0300

    Update create_ssh_tunnel.sh

--- create_ssh_tunnel.sh
+++ create_ssh_tunnel.sh
@@ -5,3 +5,7 @@ ssh -N -L 3306:10.10.10.99:3306  menachem@x.x.x.x -i k_g_1f1 -f -g yes
 
 #open prompt:
 ssh -N -L 3306:x.x.x.x:3306  menachem@x.x.x.x -i k_g_1f1 -f
+
+
+#and for port :
+ssh -i data-engineer-gad-01.pem ec2-user@193.45.14.9 -N -L 9999:10.0.109.211:8000    
